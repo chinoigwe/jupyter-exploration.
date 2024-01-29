478f1f43fea129d8b8cf73a7690b602f0c5e216c
chinoigwe <157918710+chinoigwe@users.noreply.github.com>
2024-01-28 18:35:12 -0600

    Add files via upload

---
+++ My_First_Notebook.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+explanation of the cpncepts of cells in jupyter notebook:
+code cells are used for writing and executing codes
+they run and display output below the cell
+it shows In [ ]: to indicate its ready for code input
+markdown cells are used for formatted text, explanation, and documentation
+it supports markdown syntax for easy text formatting
+it displays formatted text when the cell is run
+ther is no In [ ]:, it just contains formatted text
+
+
+# In[1]:
+
+
+print("Hello, World!")
+
+
+# In[4]:
+
+
+print ("My first markdown cell in Jupyter")
+
+
+# In[ ]:
+
+
+
+
